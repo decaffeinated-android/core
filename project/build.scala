ccd02987037ce6d75814acd6e3d7f6e8fab8ec40
Daniel Solano Gómez <daniel@solanogomez.org>
2012-01-05 09:15:30 -0600

    Update to SBT 0.11.2

---
+++ project/build.scala
@@ -0,0 +1,24 @@
+import sbt._
+
+import Keys._
+import AndroidKeys._
+
+object Settings {
+  val default = Defaults.defaultSettings ++ Seq (
+    organization := "com.decaffeinatedandroid.core",
+    version := "1.0",
+    scalaVersion := "2.9.1"
+  )
+
+  lazy val android = Settings.default ++ AndroidProject.androidSettings ++ Seq (
+    platformName in Android := "android-8"
+  )
+}
+
+object AndroidBuild extends Build {
+  lazy val helloAndroid = Project (
+    "hello-android",
+    file("hello-android"),
+    settings = Settings.android
+  )
+}
